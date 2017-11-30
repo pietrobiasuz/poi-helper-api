9f1bb837539b7c46af7249680e03f25bfeb8e9a3
Pietro Biasuz <pietro.biasuz@procempa.com.br>
2017-11-30 16:03:09 -0200

    Altera nome DataFormat para DataFormats

--- src/main/java/org/poihelper/sheet/DataFormats.java
+++ src/main/java/org/poihelper/sheet/DataFormats.java
@@ -1,6 +1,6 @@
 package org.poihelper.sheet;
 
-public class DataFormat {
+public class DataFormats {
     public static final String CURRENCY_BR = "R$ #,#0.00";
     public static final String DATE_BR = "dd/mm/yyyy";
     public static final String DATE_TIME_BR = "dd/mm/yyyy hh:mm:ss";
